0d7dc7388666ffe137f2e380077900a657f0de21
huashenliang <JohnnyLiang77@gmail.com>
2020-03-25 18:27:17 -0500

    Able to dynamic scroll to top when changing routes

--- client/src/hoc/layout.js
+++ client/src/hoc/layout.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 import Header from '../components/Header_footer/Header';
 import Footer from '../components/Header_footer/Footer';
+import DynamicScrollToTop from '../components/utils/dynamicScrollToTop';
 
 class Layout extends Component {
     render() {
         return (
             <div>
+                < DynamicScrollToTop />
                 <Header/>
                 <div className="page_container">
                     {this.props.children}
