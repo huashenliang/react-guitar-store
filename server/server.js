b0ff5851e965b1e486f6a9e07766295db69da9bd
huashenliang <JohnnyLiang77@gmail.com>
2020-03-27 11:16:54 -0500

    server changes

--- server/server.js
+++ server/server.js
@@ -10,11 +10,13 @@ const async = require('async');
 require('dotenv').config();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE)
+mongoose.connect(process.env.MONGODB_URI)
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+app.use(express.static('client/build'))
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -417,6 +419,13 @@ app.post('/api/users/update_profile',auth,(req,res)=>{
     );
 })
 
+//DEFUALT
+if(process.env.NODE_ENV === 'production') {
+    const path = require('path');
+    app.get('/*', (req,res) => {
+        res.sendfile(path.resolve(__dirname,'../client','build','index.html'))
+    })
+}
 
 const port = process.env.PORT || 3002;
 app.listen(port,()=>{
